64bd633f9b681d2bf8de0edff0b6cfd3763979f0
Maddie Lewis <maddieamie@gmail.com>
2023-09-05 14:32:35 -0700

    Lab 02: Created a Favorites State & styled the HornedBeast Component with a React Bootstrap Card.

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './HornedBeast.css';
-import ImgComp from './ImgComp.js';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0
+    };
+  }
+
+  countFaves= () => {
+    this.setState({ Favorites: this.state.favorites + 1 });
+  }
+
   render() {
 
     const { imageURL, idx, title , description, keyword, horns} = this.props;
  
     return (
       <article className="HornedBeast">
-        <h2>Horned Beast: {title}</h2>
-        <div>
-        <img src={imageURL} alt={keyword} title={title} idx={idx}/>
-        </div>
-
+        <Card style={{ width: '18rem' }}>
+        
+        <Card.Img variant="top" src={imageURL} alt={keyword} title={title} idx={idx}/>
+        <Card.Body>
+        <Card.Title>Horned Beast: {title}</Card.Title>
+        <Card.Text>
         <p>Description: {description}</p>
         <p>Horns: {horns}</p>
+        <Button variant="warning" onClick={this.countFaves}>Favorite this beast!</Button>
+        <span> ❤️ {this.state.favorites} Favorites</span>
+        </Card.Text>
+        </Card.Body>
+        </Card>
       </article>
     )
   }
